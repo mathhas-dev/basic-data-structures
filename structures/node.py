f8ff2f340706d3fb62e34def0431ac2ab6dd1cac
msilva@bedu.tech <msilva@bedu.tech>
2021-12-08 16:56:38 -0300

    basic structure to node

---
+++ structures/node.py
@@ -0,0 +1,10 @@
+class Node():
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+    def __repr__(self) -> str:
+        return f"data: {str(self.data)}\nnext: {str(self.next.data)}"
+
+    def __str__(self) -> str:
+        return self.data
