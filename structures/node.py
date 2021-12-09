95308d232d556f85e6ad67c68340270181a9a1a3
msilva@bedu.tech <msilva@bedu.tech>
2021-12-09 18:15:38 -0300

    Adding binary node

--- structures/node.py
+++ structures/node.py
@@ -8,3 +8,16 @@ class Node():
 
     def __str__(self) -> str:
         return str(self.data)
+
+
+class BinaryNode():
+    def __init__(self, data=None) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self) -> str:
+        return f"data: {str(self.data)}\nleft: {str(self.left.data)}\nright: {str(self.right.data)}"
+
+    def __str__(self) -> str:
+        return str(self.data)
